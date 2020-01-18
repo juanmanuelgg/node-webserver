81b6f7824477a043e0db94b1dc7a6c79bf66efce
Juan Manuel González Garzón <jmgonzalezgarzon@gmail.com>
2020-01-18 05:07:07 -0500

    A simple web server using node

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = 3000
+let app = express()
+
+app.use(express.static('public'))
+
+app.get('*', (req, res, next) => {
+  res.sendFile(path.resolve('public/index.html'))
+})
+
+app.listen(PORT, () => {
+  console.log('App listening on port 3000!')
+})

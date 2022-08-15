e37005c6babff5bcda24d428f51c783e95157e69
Juan Manuel <jmgonzalezgarzon@gmail.com>
2022-08-15 14:05:21 -0500

    pasar el server a ser un ES module y usar un rate-limiter

--- server/main.mjs
+++ server/main.mjs
@@ -1,15 +1,25 @@
-const express = require('express');
-const path = require('path');
-const webpush = require('web-push');
+import dotenv from 'dotenv';
+import express, { json, static as lol } from 'express';
+import rateLimit from 'express-rate-limit';
+import { dirname, join, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import webpush from 'web-push';
 
-require('dotenv').config();
+dotenv.config();
 
 const PORT = 9012;
 
 const subscriptionsMap = new Map();
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
-app.use(express.json());
+app.use(json());
 
 // Key creation: console.log(webpush.generateVAPIDKeys());
 webpush.setVapidDetails(
@@ -18,7 +28,9 @@ webpush.setVapidDetails(
   process.env.VAPID_PRIVATE_KEY
 );
 
-app.use(express.static(path.join(__dirname, 'public')));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(lol(join(__dirname, 'public')));
 
 app.get('/vapidPublicKey', (req, res) => {
   res.send(process.env.VAPID_PUBLIC_KEY);
@@ -50,12 +62,15 @@ app.get('/push/:message', async (req, res) => {
 
 // This 2 must be the last ones
 app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'service-worker.js'));
+  res.sendFile(resolve(__dirname, 'public', 'service-worker.js'));
 });
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(join(__dirname, 'public', 'index.html'));
 });
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 
 app.listen(PORT, console.log(`App on http://localhost:${PORT}`));
 // https://create-react-app.dev/docs/deployment/
